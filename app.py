decb84c8a1f73a4cc9437e04f3512d09a955fe99
Md Ashikul Haque <37145914+mdashikul798@users.noreply.github.com>
2020-12-12 14:00:25 +0600

    Update app.py
    Read xl file

--- app.py
+++ app.py
@@ -18,3 +18,27 @@ while True:
         break
     else:
         print("Can't understand your command")
+        
+        #read XL file
+        import openpyxl as xl
+        from openpyxl.chart import BarChart, Reference
+
+
+        wb = xl.load_workbook('transaction.xlsx')
+        sheet = wb['Sheet1']
+        for row in range(2, sheet.max_row + 1):
+           cell = sheet.cell(row, 3)
+           percentage = cell.value * 0.9
+           new_cell = sheet.cell(row, 4)
+           new_cell.value = percentage
+
+        values = Reference(sheet,
+                  min_row=2,
+                  max_row=sheet.max_row,
+                  min_col=4,
+                  max_col=4)
+        chart = BarChart()
+        chart.add_data(values)
+        sheet.add_chart(chart, 'f2')
+
+        wb.save('transaction.xlsx')
